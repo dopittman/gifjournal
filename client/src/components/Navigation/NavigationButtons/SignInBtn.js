a11550cde886b1d07bc872c2fb59a1b5a8a1146d
dopittman <dpittmancontact@gmail.com>
2020-12-14 17:33:52 -0500

    Update Sign In button to add animation and accessibility
    The Sign In button provides visual feedback for users when hovered, active, or focused.

--- client/src/components/Navigation/NavigationButtons/SignInBtn.js
+++ client/src/components/Navigation/NavigationButtons/SignInBtn.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button'
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
@@ -6,22 +6,36 @@ import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 
 const useStyles = makeStyles(() => ({
     loginBtn: {
-        backgroundColor: 'rgba(300,300,300,.2)',
+        color: '#fff',
+        backgroundColor: 'rgba(300,300,300,.18)',
         borderRadius: '16.5px',
         paddingLeft: '16px',
         paddingRight: '12px',
         padding: '.3rem',
         textTransform: 'none',
+        '&:hover': {
+            background: 'rgba(300,300,300,.5)',
       },
+    }
   }));
 
-export default function(){
+ const SignInButton = () => {
+    const [isHovered, setIsHovered] = useState(false);
 
     const classes = useStyles();
 
     return (
-        <Button className={classes.loginBtn} color='inherit'>
-            Sign In <ChevronRightIcon />
+        <Button 
+            onMouseEnter={() => { setIsHovered(true) } }
+            onMouseLeave={() => { setIsHovered(false)} }
+        className={`${classes.loginBtn} hvr-forward hvr-forward:hover hvr-forward:focus hvr-forward:active .hvr-forward-moved`} color='inherit'>
+            Sign In <ChevronRightIcon  />
         </ Button>
+
+        /* 
+        <div className={isHovered === true ? 'hvr-forward hvr-forward:hover hvr-forward:focus hvr-forward:active .hvr-forward-moved ' : 'hvr-forward'}>
+        */ 
     )
 }
+
+export default SignInButton;
